8fd6a5cf3800385886fb6a55b13d29237303e960
Daniel Pérez Solís <dpsol2008@gmail.com>
2020-06-26 18:45:08 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Daniel Pérez Solís on 26/06/20.
+//  Copyright © 2020 Artnetsoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
